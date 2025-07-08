53fc4927164187716f5af64d1da2f511e99747fe
Sumit Attri <sumitattri165@gmail.com>
2025-07-08 18:12:40 +0530

    tailwind css setup done

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+
+// import tailwindcss from "@tailwindcss/postcss";
+// import autoprefixer from "autoprefixer";
+
+// export default {
+//   plugins: [tailwindcss(), autoprefixer()],
+// };

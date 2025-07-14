da9afd04fd2a4edc44b1d84415aa7114b1b9dfdb
Sumit Attri <sumitattri165@gmail.com>
2025-07-14 23:50:08 +0530

    ..

--- api/chat.js
+++ api/chat.js
@@ -50,6 +50,10 @@
 // }
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
